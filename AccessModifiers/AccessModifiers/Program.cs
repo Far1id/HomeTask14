ba780b606f8e05c1b7a94b562a4da12b7039e3e0
Far1id <7k7u1q0@code.edu.az>
2022-03-29 07:26:50 +0400

    Created Hometask file

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Username daxil edin : ");
+            string username = Console.ReadLine();          
+            Console.WriteLine("Passwordu daxil edin");
+            string password = Console.ReadLine();
+            User user = new User(username)
+            {
+                Password = password,
+            };
+
+            user.GetInfo();
+            string check = "Salam123";
+            Console.WriteLine(user.HasLower(check));
+            Console.WriteLine(user.HasUpper(check));
+            Console.WriteLine(user.HasDigit(check));
+        }
+    }
+}
---
+++ AccessModifiers/AccessModifiers/User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModifiers
+{
+    internal class User
+    {
+        public User(string userName)
+        {
+            if (userName.Length >= 6 || userName.Length <= 25)
+                this._userName = userName;
+
+        }
+        private string _userName;
+        private string _password;
+
+        public string Username { get { return _userName; } }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value.Length >= 8 || value.Length <= 25)
+                {
+                    bool check1 = false;
+                    bool check2 = false;
+                    bool check3 = false;
+                    foreach (var item in value)
+                    {
+                        if (char.IsDigit(item))
+                            check1 = true;
+                        if (char.IsUpper(item))
+                            check2 = true;
+                        if (char.IsLower(item))
+                            check3 = true;
+                    }
+                    if (check1 && check2 && check3)
+                        this._password = value;
+                }
+
+            }
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Username : " + _userName + "Password :  " + _password);
+        }
+        public bool HasDigit(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsDigit(item)) return true;
+            }
+            return false;
+        }
+        public bool HasUpper(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsUpper(item)) return true;
+            }
+            return false;
+        }
+        public bool HasLower(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsLower(item)) return true;
+            }
+            return false;
+        }
+    }      
+}
